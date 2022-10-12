eeef7b79460d54096d541cde27e333532bc926b6
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-10-12 12:50:28 -0400

    style: Formatted with prettier

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,16 +1,19 @@
-import { clsx } from "clsx";
-import React from "react";
+import { clsx } from 'clsx';
+import React from 'react';
 export interface TextProps {
-  size?: "sm" | "md" | "lg";
+  size?: 'sm' | 'md' | 'lg';
   children: string;
 }
-export const Text: React.FC<TextProps> = ({ size = "md", children }) => {
+export const Text: React.FC<TextProps> = ({
+  size = 'md',
+  children
+}) => {
   return (
     <span
-      className={clsx("text-gray-100 fon-sans", {
-        "text-xs": size === "sm",
-        "text-sm": size === "md",
-        "text-md": size === "lg",
+      className={clsx('text-gray-100 fon-sans', {
+        'text-xs': size === 'sm',
+        'text-sm': size === 'md',
+        'text-md': size === 'lg'
       })}
     >
       {children}
