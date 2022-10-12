b673332a2d1d3e330b23add28eaf3864ed1e3ee2
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-10-12 13:28:29 -0400

    feat: <done> Added correct size and tag

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,12 +1,12 @@
 import { Slot } from '@radix-ui/react-slot';
 import { clsx } from 'clsx';
 import React from 'react';
-export interface TextProps {
+export interface HeadingProps {
   size?: 'sm' | 'md' | 'lg';
   children: React.ReactNode;
   asChild?: boolean;
 }
-export const Text: React.FC<TextProps> = ({
+export const Heading: React.FC<HeadingProps> = ({
   size = 'md',
   children,
   asChild
@@ -14,10 +14,10 @@ export const Text: React.FC<TextProps> = ({
   const Comp = asChild ? Slot : 'h2';
   return (
     <Comp
-      className={clsx('text-gray-100 fon-sans', {
-        'text-xs': size === 'sm',
-        'text-sm': size === 'md',
-        'text-md': size === 'lg'
+      className={clsx('text-gray-100 font-bold fon-sans', {
+        'text-lg': size === 'sm',
+        'text-xl': size === 'md',
+        'text-2xl': size === 'lg'
       })}
     >
       {children}
