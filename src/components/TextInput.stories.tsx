5e63f71f71027a0a7a113b9f5b355594665a9d11
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-10-12 14:14:11 -0400

    docs: <in progress> Started TextInput documentation

---
+++ src/components/TextInput.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { TextInput } from './TextInput';
+import { TextInputProps } from './TextInput';
+export default {
+  title: 'Components/TextInput',
+  component: TextInput,
+  args: {},
+  argTypes: {}
+} as Meta<TextInputProps>;
+export const Default: StoryObj<TextInputProps> = {};
