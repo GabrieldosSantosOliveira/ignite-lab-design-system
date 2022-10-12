bebe182b19d102cce75c463f132da4b252d3534d
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-10-11 20:41:25 -0400

    chore: Added dark theme to storybook and configured tailwind colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      colors: {
+        "gray-900": "#121224",
+        "gray-800": "#202024",
+        "gray-400": "#7c7c8a",
+        "gray-200": "#c4c4cc",
+        "gray-100": "#e1e1e6",
+        "cyan-500": "#81d8f7",
+        "cyan-300": "#9BE1FB",
+      },
       fontFamily: {
         sans: "Inter, sans-serif",
       },
