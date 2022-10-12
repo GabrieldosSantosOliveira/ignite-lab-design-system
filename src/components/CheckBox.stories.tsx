0e78a1d83a4298e436ca9905f6d8224672c00626
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-10-12 16:40:25 -0400

    docs: <done>Added new component usage example

--- src/components/CheckBox.stories.tsx
+++ src/components/CheckBox.stories.tsx
@@ -2,6 +2,7 @@ import { Meta, StoryObj } from '@storybook/react';
 
 import { Checkbox } from './Checkbox';
 import { CheckboxProps } from './Checkbox';
+import { Text } from './Text';
 export default {
   title: 'Components/Checkbox',
   component: Checkbox,
@@ -9,3 +10,15 @@ export default {
   argTypes: {}
 } as Meta<CheckboxProps>;
 export const Default: StoryObj<CheckboxProps> = {};
+export const WithText: StoryObj<CheckboxProps> = {
+  decorators: [
+    Story => {
+      return (
+        <div className="flex items-center gap-2">
+          {Story()}
+          <Text size="sm">Lembre de mim por 30 dias</Text>
+        </div>
+      );
+    }
+  ]
+};
