bc2146b172901437110d8eada78d22721ac42fe2
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-10-12 11:48:11 -0400

    feat: <in progress> Added clsx and radix-ui

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -1,8 +1,21 @@
 import { Text } from "./Text";
+import { Meta, StoryObj } from "@storybook/react";
+import { TextProps } from "./Text";
 export default {
   title: "Components/Text",
   component: Text,
+  args: {
+    children: "Ignite Lab Design System",
+  },
+} as Meta<TextProps>;
+export const Default: StoryObj<TextProps> = {};
+export const Small: StoryObj<TextProps> = {
+  args: {
+    size: "sm",
+  },
 };
-export const Default = {
-
-}
+export const Large: StoryObj<TextProps> = {
+  args: {
+    size: "lg",
+  },
+};
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,3 +1,19 @@
-export const Text = () => {
-  return <h1>Text</h1>;
+import { clsx } from "clsx";
+import React from "react";
+export interface TextProps {
+  size?: "sm" | "md" | "lg";
+  children: string;
+}
+export const Text: React.FC<TextProps> = ({ size = "md", children }) => {
+  return (
+    <span
+      className={clsx("text-gray-100 fon-sans", {
+        "text-xs": size === "sm",
+        "text-sm": size === "md",
+        "text-md": size === "lg",
+      })}
+    >
+      {children}
+    </span>
+  );
 };
