8405fbd456cfa1968c34a2856776e4ff1ae223c1
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-10-13 16:25:25 -0400

    feat: Added className property

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -5,20 +5,26 @@ export interface HeadingProps {
   size?: 'sm' | 'md' | 'lg';
   children: React.ReactNode;
   asChild?: boolean;
+  className?: string;
 }
 export const Heading: React.FC<HeadingProps> = ({
   size = 'md',
   children,
-  asChild
+  asChild,
+  className
 }) => {
   const Comp = asChild ? Slot : 'h2';
   return (
     <Comp
-      className={clsx('text-gray-100 font-bold fon-sans', {
-        'text-lg': size === 'sm',
-        'text-xl': size === 'md',
-        'text-2xl': size === 'lg'
-      })}
+      className={clsx(
+        'text-gray-100 font-bold fon-sans',
+        {
+          'text-lg': size === 'sm',
+          'text-xl': size === 'md',
+          'text-2xl': size === 'lg'
+        },
+        className
+      )}
     >
       {children}
     </Comp>
