a50ef1e1aa450075df75ae8871dee7ef980aa5a3
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-10-13 16:50:43 -0400

    feat: <in progress> Form creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './styles/global.css';
+import { Envelope, Lock } from 'phosphor-react';
+
 import { Heading } from './components/Heading';
 import { Text } from './components/Text';
+import { TextInput } from './components/TextInput';
 import { Logo } from './Logo';
 function App() {
   return (
-    <div className="w-screen h-screen bg-gray-900 flex items-center justify-center text-gray-100">
+    <div className="w-screen h-screen bg-gray-900 flex flex-col items-center justify-center text-gray-100">
       <header className="flex flex-col items-center">
         <Logo />
         <Heading size="lg" className="mt-4">
@@ -14,6 +17,42 @@ function App() {
           Faça login e comece a usar!
         </Text>
       </header>
+      <form className="flex flex-col gap-4 items-stretch w-full max-w-sm mt-10">
+        <label
+          htmlFor="email"
+          className="flex flex-col gap-3"
+        >
+          <Text className="font-semibold">
+            Endereço de e-mail
+          </Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Envelope />
+            </TextInput.Icon>
+            <TextInput.Input
+              id="email"
+              type="email"
+              placeholder="Digite seu e-mail"
+            />
+          </TextInput.Root>
+        </label>
+        <label
+          htmlFor="email"
+          className="flex flex-col gap-3"
+        >
+          <Text className="font-semibold">Sua senha</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Lock />
+            </TextInput.Icon>
+            <TextInput.Input
+              id="password"
+              type="password"
+              placeholder="**********"
+            />
+          </TextInput.Root>
+        </label>
+      </form>
     </div>
   );
 }
