5f5c5f88b58c8b406721d4858b069fa7a1539228
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-10-14 13:08:40 -0400

    docs: Add docs for page SingIn

---
+++ src/pages/SingIn.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { SingIn } from './SingIn';
+export default {
+  title: 'Pages/SingIn',
+  component: SingIn,
+  args: {},
+  argTypes: {}
+} as Meta;
+export const Default: StoryObj = {};
