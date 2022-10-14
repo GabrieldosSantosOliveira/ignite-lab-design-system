6a34ef18fccece55f11dd2e3d95716e0caf70a00
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-10-14 15:15:00 -0400

    feat: Add setting for msw-storybook-addon

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,14 +1,24 @@
-import { themes } from "@storybook/theming";
-import "../src/styles/global.css"
+import { themes } from '@storybook/theming';
+import '../src/styles/global.css';
+import {
+  initialize,
+  mswDecorator
+} from 'msw-storybook-addon';
+
+initialize({
+  onUnhandledRequest: 'bypass'
+});
+
+export const decorators = [mswDecorator];
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
+      date: /Date$/
+    }
   },
   docs: {
-    theme: themes.dark,
-  },
+    theme: themes.dark
+  }
 };
